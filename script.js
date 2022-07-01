751a55b1c06262ec865bf2a4e0b01f6a50a915cd
Jake Horvath <jhorvath@totalwine.com>
2022-07-01 12:04:28 -0400

    DIG-71441: Create arrays and loop through them

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function closeLoadingScreen() {
 	var loadingScreen = document.getElementsByClassName('overlay-container');
 	loadingScreen[0].style.display = 'none';
 }
+
+//DIG-71441 below
+var gitLinks = document.getElementsByTagName('a');
+for (link in gitLinks){
+	if(gitLinks[link].href){
+	console.log(gitLinks[link].href);
+	}
+}
+for (text in gitLinks){
+	if(gitLinks[text].innerText){
+	console.log(gitLinks[text].innerText);
+	}
+}
