1d7219d1ceea8674e09f8c06ecd31138172c763a
Jake Horvath <jhorvath@totalwine.com>
2022-07-01 11:21:30 -0400

    DIG-71364: Add a function to toggle hidden class on modal.

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 console.log("Engineering Training!");
 console.log("modalButton", modalButton);
+
+function openLoadingScreen() {
+	var loadingScreen = document.getElementsByClassName('overlay-container');
+	loadingScreen[0].style.display = 'block';
+}
+
+function closeLoadingScreen() {
+	var loadingScreen = document.getElementsByClassName('overlay-container');
+	loadingScreen[0].style.display = 'none';
+}
