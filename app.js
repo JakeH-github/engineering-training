614d33c6308ebd7ed2f0a31595972050738820e7
Jake Horvath <jhorvath@totalwine.com>
2022-07-22 09:27:29 -0400

    DIG-72441: Express create a get endpoint

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
 })
 
+router.get('/getJiraTickets', (req, res) => {
+    res.json({ jirasObject: [] });
+})
+
 app.use('/', router);
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.listen(process.env.port || 3000);
