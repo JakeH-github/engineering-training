2435e2a99c3ae8613338d9e1c4a0cce18bbf1ab8
Jake Horvath <jhorvath@totalwine.com>
2022-07-29 11:13:58 -0400

    DIG-72447: Express returning data from get endpoint

--- public/script.js
+++ public/script.js
@@ -20,25 +20,26 @@ const utils = {
     if(dataLoaded === false){
       openLoadingScreen();
       const dataLoading = setTimeout(printData,1000);
-      this.renderData().then((value) => {
-        document.getElementsByClassName('primaryList')[0].innerHTML = value;
-        callBack();
         const callEndpoint = async () => await fetch('/getJiraTickets').then((value) =>{
             value.json().then((data) => {
+                this.renderData(data).then((value) => {
+                document.getElementsByClassName('primaryList')[0].innerHTML = value;
                 console.log(data);
+                callBack();
+                });
             });
         });
         callEndpoint();
-      });
+
     }
     else{
       console.log("Data is already loaded.")
     }
   },
- renderData: function(){
+ renderData: function(data){
     return new Promise((resolve, reject) => {
       let response = '';
-      dataHandler.jiraObject.forEach(function(item, index) {
+      data.jirasObject.forEach(function(item, index) {
         let {icon, link, title } = item;
         response += `<li><i class="bi bi-x"></i><i class="${icon}"></i><a href="${link}">${title}</a></li>`;
       })
