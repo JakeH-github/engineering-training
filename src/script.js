51dc766d9e86958706e4e48c9890da89981c4852
Jake Horvath <jhorvath@totalwine.com>
2022-08-17 16:11:38 -0400

    DIG-72913: Configuring babel, webpack and react

---
+++ src/script.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+<React.Fragment><h1>Hello World!</h1></React.Fragment>,
+    document.getElementById('root')
+);
+
+(async function() {
+console.log("Engineering Training!");
+console.log("modalButton", modalButton);
+
+function openLoadingScreen() {
+	var loadingScreen = document.getElementsByClassName('overlay-container');
+	loadingScreen[0].style.display = 'block';
+}
+
+function closeLoadingScreen() {
+	var loadingScreen = document.getElementsByClassName('overlay-container');
+	loadingScreen[0].style.display = 'none';
+}
+
+
+const utils = {
+  loadData: function(callBack, dataLoaded){
+    if(dataLoaded === false){
+      openLoadingScreen();
+      const dataLoading = setTimeout(printData,1000);
+        const callEndpoint = async () => await fetch('/getJiraTickets').then((value) =>{
+            value.json().then((data) => {
+                this.renderData(data).then((value) => {
+                document.getElementsByClassName('primaryList')[0].innerHTML = value;
+                console.log(data);
+                callBack();
+                });
+            });
+        });
+        callEndpoint();
+
+    }
+    else{
+      console.log("Data is already loaded.")
+    }
+  },
+ renderData: function(data){
+    return new Promise((resolve, reject) => {
+      let response = '';
+      data.jirasObject.forEach(function(item, index) {
+        let {icon, link, title } = item;
+        response += `<li><i class="bi bi-x"></i><i class="${icon}"></i><a href="${link}">${title}</a></li>`;
+      })
+      resolve(response);
+    });
+  }
+}
+
+
+function printData(){
+	closeLoadingScreen();
+	console.log("Data loaded");
+}
+
+const initModalButton=()=>{
+	return new Promise((resolve, reject) => {
+		let dataLoaded = false;
+		console.log("dataLoaded", dataLoaded)
+		modalButton.addEventListener("click", ()=>{
+			utils.loadData(()=>{
+				dataLoaded=true;
+				resolve(dataLoaded);
+			},dataLoaded);
+		})
+	})
+}
+
+
+console.log("BEFORE initModalButton is called");
+await initModalButton();
+console.log("AFTER initModalButton is called");
+
+})();
