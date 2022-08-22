f49d114686e91a73fd9c452ba34161f02e06a924
Jake Horvath <jhorvath@totalwine.com>
2022-08-22 09:23:11 -0400

    DIG-72965: Create new utils module

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Button from './components/button';
+import utils from './utils'
 
 ReactDOM.render(
 <Button/>,
@@ -11,55 +12,11 @@ ReactDOM.render(
 console.log("Engineering Training!");
 console.log("modalButton", modalButton);
 
-function openLoadingScreen() {
-	var loadingScreen = document.getElementsByClassName('overlay-container');
-	loadingScreen[0].style.display = 'block';
-}
-
-function closeLoadingScreen() {
-	var loadingScreen = document.getElementsByClassName('overlay-container');
-	loadingScreen[0].style.display = 'none';
-}
 
 
-const utils = {
-  loadData: function(callBack, dataLoaded){
-    if(dataLoaded === false){
-      openLoadingScreen();
-      const dataLoading = setTimeout(printData,1000);
-        const callEndpoint = async () => await fetch('/getJiraTickets').then((value) =>{
-            value.json().then((data) => {
-                this.renderData(data).then((value) => {
-                document.getElementsByClassName('primaryList')[0].innerHTML = value;
-                console.log(data);
-                callBack();
-                });
-            });
-        });
-        callEndpoint();
-
-    }
-    else{
-      console.log("Data is already loaded.")
-    }
-  },
- renderData: function(data){
-    return new Promise((resolve, reject) => {
-      let response = '';
-      data.jirasObject.forEach(function(item, index) {
-        let {icon, link, title } = item;
-        response += `<li><i class="bi bi-x"></i><i class="${icon}"></i><a href="${link}">${title}</a></li>`;
-      })
-      resolve(response);
-    });
-  }
-}
 
 
-function printData(){
-	closeLoadingScreen();
-	console.log("Data loaded");
-}
+
 
 const initModalButton=()=>{
 	return new Promise((resolve, reject) => {
---
+++ src/utils.js
@@ -0,0 +1,49 @@
+function openLoadingScreen() {
+    var loadingScreen = document.getElementsByClassName('overlay-container');
+    loadingScreen[0].style.display = 'block';
+}
+
+function printData(){
+    closeLoadingScreen();
+    console.log("Data loaded");
+}
+
+function closeLoadingScreen() {
+    var loadingScreen = document.getElementsByClassName('overlay-container');
+    loadingScreen[0].style.display = 'none';
+}
+
+const utils = {
+    loadData: function(callBack, dataLoaded){
+        if(dataLoaded === false){
+            openLoadingScreen();
+            const dataLoading = setTimeout(printData,1000);
+            const callEndpoint = async () => await fetch('/getJiraTickets').then((value) =>{
+                value.json().then((data) => {
+                    this.renderData(data).then((value) => {
+                        document.getElementsByClassName('primaryList')[0].innerHTML = value;
+                        console.log(data);
+                        callBack();
+                    });
+                });
+            });
+            callEndpoint();
+
+        }
+        else{
+            console.log("Data is already loaded.")
+        }
+    },
+    renderData: function(data){
+        return new Promise((resolve, reject) => {
+            let response = '';
+            data.jirasObject.forEach(function(item, index) {
+                let {icon, link, title } = item;
+                response += `<li><i class="bi bi-x"></i><i class="${icon}"></i><a href="${link}">${title}</a></li>`;
+            })
+            resolve(response);
+        });
+    }
+}
+
+export default utils;
